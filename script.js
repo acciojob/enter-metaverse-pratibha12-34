71be04bf0db616ca1a27e09bc06b894405a063af
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-25 03:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let btn= document.getElementById("enterBtn")
 btn.addEventListener("click",(e)=>{
 	e.preventDefault();
 	let h1= document.createElement('h1');
+	p.innerText=null;
 	h1.innerText="Entered Metaverse";
+	p.append(h1);
 	
 })
