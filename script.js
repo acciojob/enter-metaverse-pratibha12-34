a1fc54425bc57c89803e65f504cedae1ab800213
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-25 03:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-let p= document.getElementByID("status")
-let btn= document.getElementByID("enterBtn")
+let p= document.getElementById("status")
+let btn= document.getElementById("enterBtn")
 btn.addEventListener("click",(e)=>{
 	e.preventDefault();
 	let h1= document.createElement('h1');
